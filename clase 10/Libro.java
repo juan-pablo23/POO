71086a424cbc6a082a0ae37429e4308fcad0d024
juan-pablo23 <juan.chaparroj@campusucc.edu.co>
2025-10-11 21:32:21 +0000

    cambios

--- clase 10/Libro.java
+++ clase 10/Libro.java
@@ -1,3 +1,13 @@
 public class Libro {
-    
+    private string titulo;
+    private string autor; 
+    private int anioPublicacion;
+    private int numeroPaginas;
+    private boolean prestado; 
+
+    public Libro(string titulo, string autor, int anioPublicacion, int numeroPaginas){
+        this.titulo = titulo; 
+        this.autor = autor; 
+
+    }
 }
