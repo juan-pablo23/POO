30da011bc0de0f5ae4182bc937981ca2c564999c
juan-pablo23 <juan.chaparroj@campusucc.edu.co>
2025-11-17 18:08:02 +0000

    métodos y tostring elevador

--- ultimo momento/Elevador.java
+++ ultimo momento/Elevador.java
@@ -10,5 +10,29 @@ public class Elevador {
         this. direccion = Direccion.QUIETO;
         this. puerta  = new Puerta();
     }
-    
+    public int obtenerPisoACtual() { return pisoACtual; }
+    public Direccion obteneDireccion() { return direccion; }
+    public Puerta obtenerPuerta() { return puerta; }
+
+    public void moberUnPiso() {
+        if (direccion == Direccion.SUBIR) pisoActual++;
+        else if (direccion == Direccion.BAJAR) pisoACtual--;
+
+        System.out.println("Elevador" + id + "moviéndose al piso" + pisoActual);
+    }
+
+    public void irAPiso(int destino) {
+        if (destino == pisoActual) dereccion = Direccion.QUIETO;
+        else direccion = (detino > pisoACtual) ? Direccion.SUBIR : direccion.BAJAR;
+    }
+
+    public void llegarYAbrir() {
+        System.out.println("Elevador " + id + "llegó al piso" + pisoACtual);
+        puerta.abrir();
+    }
+
+    @Override
+    public String toString() {
+        return "Elevador[id=" + id + ", piso=" + pisoActual + ", dir=" + direccion + ", puerta=" + puerta + "]";
+    }
 }
